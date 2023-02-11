dac1572f53e6ccfe7cde7ec729e095343fb223b3
Roger Diller <roger@flexrentalsolutions.com>
2023-02-11 16:11:37 -0500

    k

--- src/main/java/com/springboot3/springboot3demo/InfoController.java
+++ src/main/java/com/springboot3/springboot3demo/InfoController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class InfoController {
 
-    record Info (String jvmVersion) {}
+    record Info (String jvmVersion, String jvmVendor) {}
 
 
     @GetMapping("/info")
     public Info info(){
 
-        return new Info(System.getProperty("java.version"));
+        return new Info(System.getProperty("java.version"), System.getProperty("java.vm.vendor"));
     }
 
 }
