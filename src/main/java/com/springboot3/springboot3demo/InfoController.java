0e234f3a0f2dcce6f66abe6118e0270ea8cf0ff2
Roger Diller <roger@flexrentalsolutions.com>
2023-02-11 16:18:33 -0500

    jvmId

--- src/main/java/com/springboot3/springboot3demo/InfoController.java
+++ src/main/java/com/springboot3/springboot3demo/InfoController.java
@@ -3,16 +3,21 @@ package com.springboot3.springboot3demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+
 @RestController
 public class InfoController {
 
-    record Info (String jvmVersion, String jvmVendor) {}
+    record Info (String jvmId, String jvmVersion, String jvmVendor) {}
 
 
     @GetMapping("/info")
     public Info info(){
 
-        return new Info(System.getProperty("java.version"), System.getProperty("java.vm.vendor"));
+        RuntimeMXBean mxBean = ManagementFactory.getRuntimeMXBean();
+
+        return new Info(mxBean.getName(), System.getProperty("java.version"), System.getProperty("java.vm.vendor"));
     }
 
 }
