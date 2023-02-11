6cb374385b52809d7d3ef2aec8a3d0c57e6abcc7
Roger Diller <roger@flexrentalsolutions.com>
2023-02-11 10:29:29 -0500

    initial commit

---
+++ src/test/java/com/springboot3/springboot3demo/SpringBoot3DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot3.springboot3demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
