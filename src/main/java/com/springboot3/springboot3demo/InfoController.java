b14ec906e230ff2da481fdae5981d5cf283479a5
Roger Diller <roger@flexrentalsolutions.com>
2023-02-11 21:48:50 -0500

    more properties

--- src/main/java/com/springboot3/springboot3demo/InfoController.java
+++ src/main/java/com/springboot3/springboot3demo/InfoController.java
@@ -1,5 +1,7 @@
 package com.springboot3.springboot3demo;
 
+import org.apache.commons.lang3.time.DurationFormatUtils;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,17 +11,22 @@ import java.lang.management.RuntimeMXBean;
 @RestController
 public class InfoController {
 
-    record Info (String jvmId, String jvmVersion, String jvmVendor) {}
+    static final String UPTIME_DURATION_FORMAT = "d' day, 'H' hour, 'm' min, 's' sec'";
 
+    record Info (String jvmId, String jvmVersion, String jvmVendor, String springBootVersion, String uptime) {}
 
     @GetMapping("/info")
     public Info info(){
 
-        RuntimeMXBean mxBean = ManagementFactory.getRuntimeMXBean();
-
-        return new Info(mxBean.getName(),
+        return new Info( ManagementFactory.getRuntimeMXBean().getName(),
                 System.getProperty("java.version"),
-                System.getProperty("java.vm.vendor"));
+                System.getProperty("java.vm.vendor"),
+                SpringBootVersion.getVersion(),
+                getUptimeString(ManagementFactory.getRuntimeMXBean().getUptime()));
+    }
+
+    static String getUptimeString(long uptime) {
+        return DurationFormatUtils.formatDuration(uptime, UPTIME_DURATION_FORMAT);
     }
 
 }
