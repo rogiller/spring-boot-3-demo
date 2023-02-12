875df16b49825cdf5d97634b2e772f7533620acd
Roger Diller <roger@flexrentalsolutions.com>
2023-02-11 21:49:50 -0500

    import

--- src/main/java/com/springboot3/springboot3demo/InfoController.java
+++ src/main/java/com/springboot3/springboot3demo/InfoController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.management.ManagementFactory;
-import java.lang.management.RuntimeMXBean;
 
 @RestController
 public class InfoController {
