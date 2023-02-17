ee4495252127490facd6157b72b286824e6c02f0
Roger Diller <roger@flexrentalsolutions.com>
2023-02-16 21:02:07 -0500

    mysql

--- src/main/java/com/springboot3/springboot3demo/InfoController.java
+++ src/main/java/com/springboot3/springboot3demo/InfoController.java
@@ -8,6 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.management.ManagementFactory;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class InfoController {
@@ -28,6 +35,31 @@ public class InfoController {
                 SpringBootVersion.getVersion(),
                 getUptimeString(ManagementFactory.getRuntimeMXBean().getUptime()),
                 buildProperties);
+    }
+    @GetMapping("/mysql")
+    public List<String> mysql(){
+
+        List<String> arrayList = new ArrayList<>();
+
+        try {
+            String dbUrl = System.getenv().get("BOOT3_DB_URL");
+            String dbUser = System.getenv().get("BOOT3_DB_USER");
+            String dbPw = System.getenv().get("BOOT3_DB_PW");
+
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(dbUrl, dbUser, dbPw) ;
+            Statement stmt = conn.createStatement() ;
+            String query = "select location_code from st_biz_location order by location_code";
+            ResultSet rs = stmt.executeQuery(query) ;
+            while(rs.next()){
+                arrayList.add(rs.getString(1));
+            }
+            return arrayList;
+
+        }catch(Exception e){
+            arrayList.add(e.getMessage());
+            return arrayList;
+        }
     }
 
     static String getUptimeString(long uptime) {
